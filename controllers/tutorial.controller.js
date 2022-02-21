9005d68391f9a5e964a6273a98b62d8042fc63be
RakeshG <r@gmail.com>
2022-02-21 14:27:43 +0530

    Finding all Objects by Condition

--- controllers/tutorial.controller.js
+++ controllers/tutorial.controller.js
@@ -128,3 +128,18 @@ exports.deleteAll = (req, res) => {
       });
     });
 };
+
+//Finding all Objects by Condition
+exports.findAllPublished = (req, res) => {
+  Tutorial.findAll({ where: { published: true } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials.",
+      });
+    });
+};
+
